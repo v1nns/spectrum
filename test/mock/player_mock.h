34f80e9b0ee20a708cea5859b76c42d77dcb7a0b
Vinicius Longaray <vmouralongaray@gmail.com>
2022-11-24 18:08:14 -0300

    Add mock for Player API

---
+++ test/mock/player_mock.h
@@ -0,0 +1,28 @@
+/**
+ * \file
+ * \brief  Mock class for Player API
+ */
+
+#ifndef INCLUDE_TEST_MOCK_PLAYER_MOCK_H_
+#define INCLUDE_TEST_MOCK_PLAYER_MOCK_H_
+
+#include <gmock/gmock-function-mocker.h>
+
+#include "audio/player.h"
+
+namespace {
+
+class PlayerMock final : public audio::AudioControl {
+ public:
+  MOCK_METHOD(void, Play, (const std::string& filepath), (override));
+  MOCK_METHOD(void, PauseOrResume, (), (override));
+  MOCK_METHOD(void, Stop, (), (override));
+  MOCK_METHOD(void, SetAudioVolume, (model::Volume value), (override));
+  MOCK_METHOD(model::Volume, GetAudioVolume, (), (override));
+  MOCK_METHOD(void, SeekForwardPosition, (int value), (override));
+  MOCK_METHOD(void, SeekBackwardPosition, (int value), (override));
+  MOCK_METHOD(void, Exit, (), (override));
+};
+
+}  // namespace
+#endif  // INCLUDE_TEST_MOCK_PLAYER_MOCK_H_
